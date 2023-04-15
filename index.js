02235ac321a5e323a45212e28c854495b1f334a1
Leonardo Reyes <reyesleonardo44@yahoo.com>
2023-04-14 20:52:39 -0700

    advanced functions lab complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = arrayDrivers => arrayDrivers.slice(0,2);
+
+const returnLastTwoDrivers = arrayDrivers => arrayDrivers.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (fare) => {
+    const fareMultiplier = multiplier => multiplier * fare;
+    return fareMultiplier;
+}
+
+const fareDoubler = fare => createFareMultiplier(fare)(2);
+
+const fareTripler = fare => createFareMultiplier(fare)(3);
+
+const selectDifferentDrivers = (arrayDrivers, chosenFunction) => {
+    if (chosenFunction === returnFirstTwoDrivers) {
+       return returnFirstTwoDrivers(arrayDrivers);
+    } else {
+        return returnLastTwoDrivers(arrayDrivers);
+    }
+}
